28a83db89d273822ed79422246dfa6a654ddd71e
MariaPaulaAlcantara <mariapaula.contato1@gmail.com>
2024-08-05 18:29:22 -0300

    correções no código

--- scripts.js
+++ scripts.js
@@ -1,126 +1,116 @@
-$(document).ready(function () {
-    var lookup = {
-        'M': 1000,
-        'CM': 900,
-        'D': 500,
-        'CD': 400,
-        'C': 100,
-        'XC': 90,
-        'L': 50,
-        'XL': 40,
-        'X': 10,
-        'IX': 9,
-        'V': 5,
-        'IV': 4,
-        'I': 1
+class ConversorRomanos {
+  constructor() {
+    this.tabelaConversao = {
+      'M̅': 1000000,
+      'D̅': 500000,
+      'C̅': 100000,
+      'L̅': 50000,
+      'X̅': 10000,
+      'V̅': 5000,
+      'M': 1000,
+      'CM': 900,
+      'D': 500,
+      'CD': 400,
+      'C': 100,
+      'XC': 90,
+      'L': 50,
+      'XL': 40,
+      'X': 10,
+      'IX': 9,
+      'V': 5,
+      'IV': 4,
+      'I': 1
     };
+  }
 
-    function converterNumRomano(num) {
-        var roman = '';
-        for (var i in lookup) {
-            while (num >= lookup[i]) {
-                roman += i;
-                num -= lookup[i];
-            }
-        }
-        return roman;
+  converterParaRomano(numero) {
+    let romano = '';
+    for (let chave in this.tabelaConversao) {
+      while (numero >= this.tabelaConversao[chave]) {
+        romano += chave;
+        numero -= this.tabelaConversao[chave];
+      }
     }
+    return romano;
+  }
 
-    function converterNumDecimal(roman) {
-        var num = 0;
-        var i = 0;
-        while (i < roman.length) {
-            var char1 = roman[i];
-            var char2 = roman[i + 1];
-            var twoChar = char1 + char2;
-            var value1 = lookup[char1] || 0;
-            var value2 = lookup[twoChar] || 0;
-            if (value2 > value1) {
-                num += value2;
-                i += 2;
-            } else {
-                num += value1;
-                i++;
-            }
-        }
-        return num;
+  converterParaDecimal(romano) {
+    let numero = 0;
+    let i = 0;
+    while (i < romano.length) {
+      let caractere1 = romano[i];
+      let caractere2 = romano[i + 1];
+      let doisCaracteres = caractere1 + caractere2;
+      let valor1 = this.tabelaConversao[caractere1] || 0;
+      let valor2 = this.tabelaConversao[doisCaracteres] || 0;
+      if (valor2 > valor1) {
+        numero += valor2;
+        i += 2;
+      } else {
+        numero += valor1;
+        i++;
+      }
     }
+    return numero;
+  }
 
-    function corrigirNumeroRomano(roman) {
-        var romanos = Object.keys(lookup).reverse();
-        var num = 0;
-        var i = 0;
-        while (i < roman.length) {
-            var char1 = roman[i];
-            var char2 = roman[i + 1];
-            var twoChar = char1 + char2;
-            var value1 = lookup[char1] || 0;
-            var value2 = lookup[twoChar] || 0;
-            if (value2 > value1) {
-                num += value2;
-                i += 2;
-            } else {
-                num += value1;
-                i++;
-            }
-        }
-        var romanCorreto = '';
-        for (var i in lookup) {
-            while (num >= lookup[i]) {
-                romanCorreto += i;
-                num -= lookup[i];
-            }
-        }
-        return romanCorreto;
+  corrigirNumeroRomano(romano) {
+    return this.converterParaRomano(this.converterParaDecimal(romano));
+  }
+
+  validarDecimal(numero) {
+    return !isNaN(numero) && numero >= 1 && numero <= 399999;
+  }
+
+  mostrarAlerta(mensagem, tipo = 'error') {
+    Swal.fire({
+      icon: tipo,
+      title: tipo === 'error' ? 'Erro' : 'Informação',
+      text: mensagem
+    });
+  }
+}
+
+class InterfaceConversor {
+  constructor() {
+    this.conversor = new ConversorRomanos();
+    this.inicializarEventos();
+  }
+
+  inicializarEventos() {
+    $("#converter_decimal").click(() => this.converterDecimal());
+    $("#converter_romano").click(() => this.converterRomano());
+  }
+
+  converterDecimal() {
+    let numero = parseInt($("#entrada_numero_decimal").val(), 10);
+    if (this.conversor.validarDecimal(numero)) {
+      $("#entrada_numero_romano").val(this.conversor.converterParaRomano(numero));
+    } else {
+      $("#entrada_numero_romano").val('');
+      this.conversor.mostrarAlerta('O valor deve estar entre 1 e 399999.');
     }
+  }
 
-    $("#converter_decimal").click(function () {
-        var num = parseInt($("#input_num_decimal").val(), 10);
-        console.log(num, 'teste!');
-        if (!isNaN(num) && num >= 1 && num <= 399999) {
-            $("#roman").val(converterNumRomano(num));
-        } else if (isNaN(num)) {
-            Swal.fire({
-                icon: 'info',
-                title: 'Valor não pode ser vazio',
-                text: 'Coloque um valor em um dos campos',
-            });
-        }
-        else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Valor não suportado',
-                text: 'O valor deve estar entre 1 e 399999.',
-            });
-        }
-    });
+  converterRomano() {
+    let romano = $("#entrada_numero_romano").val().toUpperCase();
+    let numero = this.conversor.converterParaDecimal(romano);
+    let romanoCorrigido = this.conversor.corrigirNumeroRomano(romano);
 
-    $("#converter_romano").click(function () {
-        var roman = $("#roman").val().toUpperCase();
-        var convertedNum = converterNumDecimal(roman);
-        if (convertedNum > 0 && convertedNum <= 399999) {
-            $("#input_num_decimal").val(convertedNum);
-        } else {
-            var romanCorreto = corrigirNumeroRomano(roman);
-            if (romanCorreto) {
-                $("#roman").val(romanCorreto);
-                $("#input_num_decimal").val(converterNumDecimal(romanCorreto));
-            } else {
-                $("#input_num_decimal").val('');
-                $("#roman").val('');
-            }
-        }
-    });
+    if (!this.conversor.validarDecimal(numero)) {
+      $("#entrada_numero_decimal").val('');
+      $("#entrada_numero_romano").val('');
+      this.conversor.mostrarAlerta('O número Romano inserido é inválido.');
+    } else {
+      $("#entrada_numero_decimal").val(numero);
+      if (romano !== romanoCorrigido) {
+        this.conversor.mostrarAlerta(`O número Romano inserido foi corrigido para ${romanoCorrigido} e corresponde ao valor ${numero}.`, 'info');
+        $("#entrada_numero_romano").val(romanoCorrigido);
+      }
+    }
+  }
+}
 
-    $("#input_num_decimal").keypress(function (e) {
-        if (e.which === 13) {
-            $("#converter_decimal").click();
-        }
-    });
-
-    $("#roman").keypress(function (e) {
-        if (e.which === 13) {
-            $("#converter_romano").click();
-        }
-    });
+$(document).ready(() => {
+  new InterfaceConversor();
 });
